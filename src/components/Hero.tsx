e7b469d1017d5457f51000e3ceb1217c06073944
asmaiqbal85 <asmaiqbal85@gmail.com>
2024-10-09 08:14:59 -0700

    updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,30 @@
+"use client"
+
 import React from 'react'
 import Image from 'next/image'
+import CustomButton from './CustomButton'
+
+
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className='hero'>
 <div className='flex-l pt-36 padding-x'>
 <h1 className='hero__title'>
-    
+    Find, book, or rent a car - quckely
+    and easily!
 </h1>
+
+<p className='hero__subtitle'>Streamline your car rental experience with our effortless booking process.</p>
+  
+<CustomButton
+title="Explore Cars"
+containersStyles="bg-primary-blue
+text-width rounded-full mt-10"
+handleClick={handleScroll}
+ />
 </div>
     </div>
   )
